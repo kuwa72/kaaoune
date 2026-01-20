ff91a5c5c92b0d3fc9e7c35f5859de10e3037486
kuwa72 <ykuwashima@gmail.com>
2026-01-20 14:14:16 +0900

    update document

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
+  allowedDevOrigins: ["http://192.168.0.188:3000"],
 };
 
 export default nextConfig;
